0c0d0b3d867370147bccf4ed5b56b462f3cfca0d
Sophia Luinenburg <videogamefpslover@gmail.com>
2026-06-08 20:13:49 -0400

    More granular errors

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import sys
+from enum import Enum
+
+class Error(Enum):
+    DIV_ZERO = "DIV_ZERO"
+    UNS_OP = "UNS_OP"
+    BAD_EQ = "BAD_EQ"
+    UNS_VAL = "UNS_VAL"
 
 def process_operation(num1, operation, num2):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return Error.UNS_VAL
     match operation:
         case "+":
             return num1 + num2
@@ -10,15 +22,13 @@ def process_operation(num1, operation, num2):
 
         case "/":
             if num2 == 0:
-                print("Error: division by 0")
-                return None
+                return Error.DIV_ZERO
             return num1 / num2
 
         case "-":
             return num1 - num2
         case _:
-            print("Error: Operation not supported. Only valid operations: +, -, /, *")
-            return None
+            return Error.UNS_OP
 
 def unmix(mixed_symbols):
     numbers = []
@@ -27,28 +37,27 @@ def unmix(mixed_symbols):
     for symbol in mixed_symbols:
         if "+*/-".find(symbol) > -1:
             if number == "":
-                print("Error: Invalid equation. Please only enter numbers (5, 10.25) or operations (+, -, /, *).")
-                return None
+                return Error.BAD_EQ
             operations.append(symbol)
             numbers.append(float(number))
             number = ""
         elif "0123456789".find(symbol) > -1:
             number += symbol
         else:
-            print("Error: Unsupported value. Please only enter numbers (5, 10.25) or operations (+, -, /, *)")
-            return None
+            return Error.UNS_VAL
     try:
         numbers.append(float(number))
     except ValueError:
-        print("Error: Improper equation. Make sure equation begins and ends with a number")
-        return None
+        return Error.BAD_EQ
     if len(numbers) != len(operations)+1:
-        print("Error: Improper equation. Make sure equation begins and ends with a number")
-        return None
+        return Error.BAD_EQ
     return [numbers, operations]
 
 def operate(sorted_symbols):
-    if sorted_symbols == None: return None
+    try:
+        if sorted_symbols in Error: return sorted_symbols
+    except TypeError:
+        pass
     numbers = sorted_symbols[0]
     operations = sorted_symbols[1]
    
@@ -57,9 +66,8 @@ def operate(sorted_symbols):
         operation = operations[index]
         if "*/".find(operation) > -1:
             result = process_operation(numbers[index], operation, numbers[index+1])
-            if result == None:
-                print("Error: Invalid operation")
-                return None
+            if result in Error:
+                return result
             numbers[index] = result
             numbers.pop(index+1)
             operations.pop(index)
@@ -74,9 +82,8 @@ def operate(sorted_symbols):
         operation = operations[index]
         if "+-".find(operation) > -1:
             result = process_operation(numbers[index], operation, numbers[index+1])
-            if result == None:
-                print("Error: Invalid operation")
-                return None
+            if result in Error:
+                return result
             numbers[index] = result
             numbers.pop(index+1)
             operations.pop(index)
@@ -85,6 +92,9 @@ def operate(sorted_symbols):
  
     return numbers[0]
 
+
+def rep():
+    return    
 
 def main():
     if (len(sys.argv) > 2):
--- test_main.py
+++ test_main.py
@@ -7,19 +7,20 @@ def test_process_operation():
             if (b != 0):
                 assert process_operation(a, "/", b) == a/b
             else:
-                assert process_operation(a, "/", b) == None
+                assert process_operation(a, "/", b) == Error.DIV_ZERO
             assert process_operation(a, "*", b) == a*b
             assert process_operation(a, "-", b) == a-b
-    assert process_operation(a, "g", b) == None
+    assert process_operation(a, "g", b) == Error.UNS_OP
+    assert process_operation("a", "*", "b") == Error.UNS_VAL
 
 def test_unmix():
     assert unmix("10-2") == [[10, 2], ["-"]]
-    assert unmix("3-") == None
-    assert unmix("/5") == None
-    assert unmix("g-2") == None
+    assert unmix("3-") == Error.BAD_EQ
+    assert unmix("/5") == Error.BAD_EQ
+    assert unmix("g-2") == Error.UNS_VAL
     assert unmix("3") == [[3], []]
 
 def test_operate():
-    assert operate(None) == None
-    assert operate(unmix("7/0")) == None
+    assert operate(unmix("2+3")) == 5
+    assert operate(unmix("7/0")) == Error.DIV_ZERO
     assert operate(unmix("10+2/4-4*6")) == -13.5
